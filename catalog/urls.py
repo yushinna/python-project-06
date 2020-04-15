3604c3ef45f249adf6d6226cbac178d350711aa5
yushinna <42777142+yushinna@users.noreply.github.com>
2020-04-15 22:57:06 +0900

    remove files

--- catalog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'random', views.random_mineral, name='random'),
-    url(r'mineral/(?P<pk>\d+)/$', views.mineral_detail, name='detail'),
-    url(r'', views.mineral_list, name='index'),
-]
